465c46de7b4296762b083f7201a2f89363605142
feras <feras.abdulazem@gmail.com>
2020-12-17 15:00:35 +0100

    SPI

--- FCController/FCController/main.cpp
+++ FCController/FCController/main.cpp
@@ -8,21 +8,47 @@
 #include <avr/io.h>
 #include "general.h"
 
-uint8_t rData[2] = {0, 0};
-uint8_t tData[4] = {0, 0, 0, 0}
+int8_t rData[2] = {0, 0};
+	
+	
+// Inside sensor (0 - 300)
+// Outside sensor (0 - 300)
+// Current pump speed (-100 - 100)
+// Current windmill speed (0 - 100)
+int8_t tData[4] = {0, 0, 0, 0};
 
 int main(void)
 {
-	uint8_t idx = 0;
     /* Replace with your application code */
 	// 1 Messrunde
     while (1) 
     {
-
-		while(!(SPSR & (1<<SPIF)));
-		rData[idx] = SPDR;	
-		//TODO: 2 Bytes empfangen und in rData schreiben
-		//TODO: 4 Bytes aus tData senden
+		// *** TESTING *** //
+		tData[0] = 250;
+		tData[1] = 175;
+		tData[2] = -55;
+		tData[3] = 50;
+		// *** TESTING *** //		
+		
+		/************************************************************************/
+		/*			   2 Bytes empfangen und in rData schreiben                 */
+		/*						4 Bytes aus tData senden                        */
+		/************************************************************************/
+		// We will send 4 bytes so count up to 3
+		for (uint8_t idx = 0; idx < 4; idx++) {
+			
+			// Write 1st byte into register
+			SPDR = tData[idx];
+			
+			// Wait for transmission
+			while(!(SPSR & (1<<SPIF)));
+			
+			// Because only the first 2 bytes are real data => check if its the 1st or 2nd byte
+			if (idx < 2) {
+				// Read register
+				rData[idx] = SPDR;
+			}
+		}
 		
 		//TODO: Pumpe setzen
 		//TODO: Windrad setzen
